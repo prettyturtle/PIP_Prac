b8831f5af67e4afd24dacf8b3c9d3b4a6c5811c9
prettyturtle <unifconti@gmail.com>
2023-12-13 22:43:31 +0900

    Initial Commit

---
+++ PIP_Prac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PIP_Prac
+//
+//  Created by yc on 12/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
